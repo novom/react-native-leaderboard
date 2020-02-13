0e0051c55aff3ee5686119f4b3b0ad7f5062ae28
Charles-Antoine Blanchette <cblanchette@novominteractive.com>
2020-02-13 16:25:57 -0500

    Use temporary fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ListView from 'deprecated-react-native-listview';
 import {
   View,
   Text,
-  ListView,
   ViewPropTypes,
   Image,
   TouchableOpacity,
